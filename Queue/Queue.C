c8a2774ed598c6cbbee0fce091dee170830d5c83
Unique-Usman <usmanakinyemi202@gmail.com>
2023-04-11 09:27:49 +0530

    Make changes to the pop function to be able to pop element when there is only one node in the queue

--- Queue/Queue.C
+++ Queue/Queue.C
@@ -31,7 +31,13 @@ int Queue<T>::pop(){
 	if (head == NULL){
 		cout << "Nothing to be poped " << endl;
 		return 0;
-	}else{
+	}else if(head->next == NULL){
+		delete head;
+		head = NULL;
+		tail = NULL;
+		return 1;
+	}
+	else{
 		node<T> *tmp = head;
 		head = head->next;
 		delete tmp;
