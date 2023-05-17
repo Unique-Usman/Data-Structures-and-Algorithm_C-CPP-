01736f40b9779594be597ecdb28bbc208ce3de5d
Unique-Usman <usmanakinyemi202@gmail.com>
2023-05-17 22:07:40 +0530

    Updated the Chain.h file. Added the required function to execute the double linked list

--- LinkedList/DoubleLinkedList/Chain.h
+++ LinkedList/DoubleLinkedList/Chain.h
@@ -9,5 +9,14 @@
 			static void pirntNode();
 			static void insertNodeEnd();
 			static void insertNodeFront();
+        		static void insertbegin(T);
+        		static void insertend(T);
+        		static void remove(T);
+        		static void printchain();
+       			static void printReverse();
+        		static int countNodes();
+        		static void insertAfter(T,T); // inserts a node after finding the specified data
+};
+
 	};
 #endif
