7660f96f515b438687f16c917527f46bfcfa5c57
Unique-Usman <usmanakinyemi202@gmail.com>
2023-05-19 23:24:45 +0530

    Updated the Chain.h and the node.h files

--- LinkedList/DoubleLinkedList/Chain.h
+++ LinkedList/DoubleLinkedList/Chain.h
@@ -1,12 +1,13 @@
 #ifndef CHAINNODE_H
 #define CHAINNODE_H
 #include "node.h"
+#include <iostream>
 	class chainNode{
 		private:
 			static node* firstNode;
 			static node* currentNode;
 		public:
-			static void pirntNode();
+			static void printNode();
 			static void insertNodeEnd();
 			static void insertNodeFront();
         		static void insertbegin(T);
@@ -14,6 +15,7 @@
         		static void remove(T);
         		static void printchain();
        			static void printReverse();
+			static void reverseNode();
         		static int countNodes();
         		static void insertAfter(T,T); // inserts a node after finding the specified data
 };
--- LinkedList/DoubleLinkedList/node.h
+++ LinkedList/DoubleLinkedList/node.h
@@ -1,6 +1,5 @@
 #ifndef NODE_H
 #define NODE_H
-	#include <bits/stdc++.h>
 	class node{
 		int data;
 		node* next;
