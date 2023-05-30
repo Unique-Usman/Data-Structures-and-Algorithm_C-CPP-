485b80281b0352a58c768b8858736cf1fe07f4ea
Unique-Usman <usmanakinyemi202@gmail.com>
2023-05-30 23:52:20 +0530

    Change the Chain header to template mode

--- LinkedList/DoubleLinkedList/Chain.h
+++ LinkedList/DoubleLinkedList/Chain.h
@@ -2,23 +2,24 @@
 #define CHAINNODE_H
 #include "node.h"
 #include <iostream>
+template <typename T>
 using namespace std;
 	class chainNode{
 		private:
-			static node* firstNode;
-			static node* currentNode;
+			static node<T> *firstNode;
+			static node<T> *currentNode;
 		public:
 			static void printNode();
 			static void insertNodeEnd();
-			static void insertNodeFront(int);
-        		static void insertbegin(int);
-        		static void insertend(int);
-        		static void remove(int);
+			static void insertNodeFront(T);
+        		static void insertbegin(T);
+        		static void insertend(T);
+        		static void remove(T);
         		static void printchain();
        			static void printReverse();
 			static void reverseNode();
         		static int countNodes();
-        		static void insertAfter(int,int); // inserts a node after finding the specified data
+        		static void insertAfter(T,T); // inserts a node after finding the specified data
 };
 
 #endif
